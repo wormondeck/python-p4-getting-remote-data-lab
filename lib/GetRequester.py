1ca5a9600f1e447011b7d632d503ee78320d4903
Ronney Arias <ronneyarias@gmail.com>
2024-10-25 21:14:20 -0500

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()
